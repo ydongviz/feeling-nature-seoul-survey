31310885a9913858bd31946b3ec8d8f917ff4347
Yinan <ydongviz@mit.edu>
2025-09-18 21:17:55 -0400

    landing video sync

--- public/tv1/app.js
+++ public/tv1/app.js
@@ -1296,7 +1296,7 @@ async function startLandingAnimationSequence() {
         'BS Map: the distribution of nature-based elements in Seoul urban environment.',
         true
       );
-      await waitForVideoTime(video, 4);
+      await wait(4000);
 
       if (!app.landing.active || app.mode !== Modes.LANDING) break;
 
@@ -1306,7 +1306,7 @@ async function startLandingAnimationSequence() {
         'BP Map: the strength of perceived Biophilia in the city',
         true
       );
-      await waitForVideoTime(video, 8);
+      await wait(4000); 
 
       if (!app.landing.active || app.mode !== Modes.LANDING) break;
 
